41c418bbd8bc413878ff119c9f299fce8e329f2a
molesir <1063441425@qq.com>
2025-01-02 18:26:26 +0800

    Add base GraphicsView

---
+++ source/widget/frame/graphicsview.cc
@@ -0,0 +1,57 @@
+#include "./graphicsview.h"
+#include "qnamespace.h"
+
+namespace kiwi::widget {
+
+    GraphicsView::GraphicsView(QWidget* parent)
+        : QGraphicsView{parent},
+        _isPanning{false} 
+    {
+        this->setDragMode(QGraphicsView::RubberBandDrag);
+        this->setInteractive(true);
+        this->setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+    }
+
+    void GraphicsView::wheelEvent(QWheelEvent* event) {
+        if (event->modifiers() & Qt::ControlModifier) {
+            const double scaleFactor = 1.15;
+            if (event->angleDelta().y() > 0) {
+                scale(scaleFactor, scaleFactor);  // 放大
+            } else {
+                scale(1.0 / scaleFactor, 1.0 / scaleFactor);  // 缩小
+            }
+        }
+    }
+
+    void GraphicsView::mousePressEvent(QMouseEvent* event) {
+        if (event->button() == Qt::MiddleButton || event->button() == Qt::RightButton) {
+            this->_isPanning = true;
+            this->_lastMousePos = event->pos();
+            this->setDragMode(QGraphicsView::NoDrag);
+            this->setCursor(Qt::ClosedHandCursor);
+        }
+
+        QGraphicsView::mousePressEvent(event);
+    }
+
+    void GraphicsView::mouseMoveEvent(QMouseEvent* event) {
+        if (this->_isPanning) {
+            auto delta = event->pos() - this->_lastMousePos;
+            this->_lastMousePos = event->pos();
+            this->translate(delta.x(), delta.y());
+        }
+
+        QGraphicsView::mouseMoveEvent(event);
+    }
+
+    void GraphicsView::mouseReleaseEvent(QMouseEvent* event) {
+        if (this->_isPanning == true) {
+            this->_isPanning = false;
+            this->setDragMode(QGraphicsView::RubberBandDrag);
+            this->setCursor(Qt::ArrowCursor);
+        }
+
+        QGraphicsView::mouseReleaseEvent(event);
+    }
+
+}
---
+++ source/widget/frame/graphicsview.h
@@ -0,0 +1,26 @@
+#include <QGraphicsView>
+#include <QWheelEvent>
+#include <QMouseEvent>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsItem>
+
+namespace kiwi::widget {
+
+    class GraphicsView : public QGraphicsView {
+        Q_OBJECT
+
+    public:
+        explicit GraphicsView(QWidget* parent = nullptr);
+
+    protected:
+        void wheelEvent(QWheelEvent* event) override;
+        void mousePressEvent(QMouseEvent* event) override;
+        void mouseMoveEvent(QMouseEvent* event) override;
+        void mouseReleaseEvent(QMouseEvent* event) override;
+
+    protected:
+        bool _isPanning; 
+        QPoint _lastMousePos;
+    };
+
+}
